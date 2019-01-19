7743753c28a4cdceae5da3441682d58d375d329b
aditya27yadav <33394875+aditya27yadav@users.noreply.github.com>
2019-01-19 23:33:47 +0530

    add project

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartGame : MonoBehaviour
+{
+    // respond on collisions
+    void OnCollisionEnter(Collision newCollision)
+    {
+        // only do stuff if hit by a projectile
+        if (newCollision.gameObject.tag == "Projectile")
+        {
+            // call the RestartGame function in the game manager
+            GameManager.gm.RestartGame();
+        }
+    }
+
+}
